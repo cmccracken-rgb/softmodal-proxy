7345c92a868f6b47a16c1af6ec6eb79735e9d943
cmccracken-rgb <cmccracken@avenuelogistics.com>
2026-04-17 17:07:07 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,27 +1,13 @@
-// src/server.js
-
 import express from "express";
-import { fetchQuote } from "./softmodal.js";
 
 const app = express();
 
-app.get("/quote", async (req, res) => {
-  try {
-    const { origin, destination, size } = req.query;
+app.get("/", (req, res) => {
+  res.send("SERVER IS WORKING");
+});
 
-    console.log("Incoming request:", req.query);
-
-    const data = await fetchQuote({ origin, destination, size });
-
-    res.json(data);
-
-  } catch (err) {
-    console.error("SERVER ERROR:", err);
-    res.status(500).json({
-      error: err.message,
-      stack: err.stack
-    });
-  }
+app.get("/quote", (req, res) => {
+  res.json({ ok: true });
 });
 
 app.listen(3000, () => {
